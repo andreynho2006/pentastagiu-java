0742fd1fec83595368671b8289049d5f488347d4
andreynho2006 <andreynho2006@gmail.com>
2018-12-14 16:00:16 +0200

    Java program that will test whether a string ends with another string

---
+++ week2/StringEndsWithString.java
@@ -0,0 +1,21 @@
+/**
+ * File: StringEndsWithString.java
+ * 
+ *  Java program that will test whether a string ends with another string
+ * 
+ * @author Andrei Cirlan
+ * 14 Dec 201812:25:31
+
+ */
+
+public class StringEndsWithString {
+
+	public static void main(String[] args) {
+		String base = "Hello java world";
+		String test = "orld";
+		
+		System.out.println(base.endsWith(test));
+		
+	}
+}
+
