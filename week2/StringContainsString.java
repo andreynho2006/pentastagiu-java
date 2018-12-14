a429f745784550de79bcb492fda6b3c973ef6e4f
andreynho2006 <andreynho2006@gmail.com>
2018-12-14 15:48:33 +0200

    Java program that test if a given String contains another String.

---
+++ week2/StringContainsString.java
@@ -0,0 +1,22 @@
+/**
+ * File: StringContainsString.java
+ * 
+ *  Java program that test if a given String contains another String.
+ * 
+ * @author Andrei Cirlan
+ * 14 Dec 201812:25:31
+
+ */
+
+
+public class StringContainsString {
+
+	public static void main(String[] args) {
+		String base = "Hello java world";
+		String test = "java";
+		
+		System.out.println(base.contains(test));
+	}	
+}
+
+
