471ccc9fa66011d19e5c5adb4d379e22a71c6285
andreynho2006 <andreynho2006@gmail.com>
2018-12-14 11:17:53 +0200

    refactor code appearence

--- week2/SumNumbers.java
+++ week2/SumNumbers.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class SumNumbers {
 
 	public static void main(String[] args) {
-		int sum = 0;										// Variabila pentru a stoca suma totala
-		System.out.printf("Adauga un numar :  ");			
+		int sum = 0;                                 // Variabila pentru a stoca suma totala
+		System.out.println("Adauga un numar :  ");			
 		Scanner in = new Scanner(System.in);				
 		
-		int numnber = in.nextInt();							// Numarul de tip integer adaugat
+		int numnber = in.nextInt();                  // Numarul de tip integer adaugat
         		
-        for(int i = 0; i <= numnber; i++) {					// iteratie pentru a calcula suma tuturor numerelor de la 0 pana la integerul adaugat inclusiv
-            sum += i;										// la fiecare iteratie adaugam integerul la suma totala
+        for(int i = 0; i <= numnber; i++) {          // iteratie pentru a calcula suma tuturor numerelor de la 0 pana la integerul adaugat inclusiv
+            sum += i;                                // la fiecare iteratie adaugam integerul la suma totala
         }
-    System.out.println(sum);								// printeaza in consola suma
+    System.out.println(sum);                        // printeaza in consola suma
 	}
 
 }
